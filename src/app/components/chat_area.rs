a1393b0940977ff06bd4877937e4c308f4c4998d
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-08-08 15:30:54 +0200

    feat: Create components folder and add custom components

---
+++ src/app/components/chat_area.rs
@@ -0,0 +1,29 @@
+use leptos::{*, html::Div};
+use crate::model::conversation::Conversation;
+
+const USER_MESSAGE_CLASS: &str = "user";
+const MODEL_MESSAGE_CLASS: &str = "model";
+
+#[component]
+pub fn ChatArea(cx: Scope, conversation: ReadSignal<Conversation>) -> impl IntoView {
+    let chat_div_ref = create_node_ref::<Div>(cx);
+    create_effect(cx, move |_| {
+      conversation.get();
+      if let Some(div) = chat_div_ref.get() {
+        div.set_scroll_top(div.scroll_height());
+      }
+    });
+    view! { cx,
+        <div class="msg" node_ref=chat_div_ref>
+          {move || conversation.get().messages.iter().map(move |message| {
+              let class_str = if message.user { USER_MESSAGE_CLASS } else { MODEL_MESSAGE_CLASS };
+              view! {cx,
+                <div class={class_str}>
+                  {message.text.clone()}
+                </div>
+              }
+            }).collect::<Vec<_>>()
+          }
+        </div>
+    }
+}
