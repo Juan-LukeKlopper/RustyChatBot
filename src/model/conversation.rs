cd68cf4edba7a161d5cf65968314a284230716c2
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-08-08 13:46:55 +0200

    feat: Add serde imports to coversation.rs

--- src/model/conversation.rs
+++ src/model/conversation.rs
@@ -1,6 +1,11 @@
-pub struct Conversation {
-    pub messages: Vec<Message>
+use serde::{ Serialize, Deserialize };
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Conversation {
+    pub messages: Vec<Message>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Message {
     pub user: bool,
     pub text: String,
